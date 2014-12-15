1b958fa27ea32bc704e7bdb0145b24ae0ac4901c
yue zheng <yuezheng@yues-MacBook-Pro.local>
2014-12-14 17:10:20 -0800

    adding bridging-header file

---
+++ X/X/X-Bridging-Header.h
@@ -0,0 +1,15 @@
+//
+//  X-Bridging-Header.h
+//  X
+//
+//  Created by yue zheng on 12/14/14.
+//  Copyright (c) 2014 PPC. All rights reserved.
+//
+
+#ifndef X_X_Bridging_Header_h
+#define X_X_Bridging_Header_h
+
+#import <Parse.h>
+#import <ParseUI.h>
+
+#endif
