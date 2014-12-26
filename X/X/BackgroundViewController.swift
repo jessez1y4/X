8bc4e620b123a0122bcee33d83857272635e0167
Yu <yu89126@gmail.com>
2014-12-25 21:40:19 -0800

    fwehflwehfeo

--- X/X/BackgroundViewController.swift
+++ X/X/BackgroundViewController.swift
@@ -18,13 +18,75 @@ class BackgroundViewController: UIViewController {
         
         var backgroundImage = UIImage(named: "mushi.jpg")
         backgroundImageView = UIImageView(frame: view.frame)
-        backgroundImageView.image = backgroundImage
-        backgroundView = UIView(frame: view.frame)
-        backgroundView.backgroundColor = UIColor(red: 43, green: 43, blue: 50, alpha: 0.5)
+//        backgroundImageView.image = backgroundImage
+//        backgroundView = UIView(frame: view.frame)
+//        backgroundView.backgroundColor = UIColor(red: 43, green: 43, blue: 50, alpha: 0.5)
+//        view.addSubview(backgroundImageView)
+//        view.addSubview(backgroundView)
+//        view.sendSubviewToBack(backgroundView)
+        
+        
+        
+//        
+//        CGContextRef context = UIGraphicsGetCurrentContext();
+//        CGContextSaveGState(context);
+//        
+//        // Draw picture first
+//        //
+//        CGContextDrawImage(context, self.frame, self.image.CGImage);
+//        
+//        // Blend mode could be any of CGBlendMode values. Now draw filled rectangle
+//        // over top of image.
+//        //
+//        CGContextSetBlendMode (context, kCGBlendModeMultiply);
+//        CGContextSetFillColor(context, CGColorGetComponents(self.overlayColor.CGColor));
+//        CGContextFillRect (context, self.bounds);
+//        CGContextRestoreGState(context);
+        
+        //We want to keep alpha, set opaque to NO; Use 0.0f for scale to use the scale factor of the device’s main screen.
+//        UIGraphicsBeginImageContextWithOptions(self.size, NO, 0.0f);
+//        [tintColor setFill];
+//        CGRect bounds = CGRectMake(0, 0, self.size.width, self.size.height);
+//        UIRectFill(bounds);
+//        
+//        //Draw the tinted image in context
+//        [self drawInRect:bounds blendMode:blendMode alpha:1.0f];
+//        
+//        UIImage *tintedImage = UIGraphicsGetImageFromCurrentImageContext();
+//        UIGraphicsEndImageContext();
+//        
+//        return tintedImage;
+        
+        
+        
+        UIGraphicsBeginImageContextWithOptions(view.frame.size, false, 0.0)
+        
+        let color = UIColor(red: 43, green: 43, blue: 250, alpha: 1)
+//        color.setFill()
+//        let bounds = view.frame
+//        UIRectFill(bounds)
+        
+//        backgroundImage?.drawInRect(bounds, blendMode: kCGBlendModeOverlay, alpha: 0.2)
+        
+        let context = UIGraphicsGetCurrentContext()
+//        CGContextSaveGState(context)
+//        CGContextTranslateCTM(context, 0, -view.frame.size.height);
+//        CGContextScaleCTM(context, 1.0, -1.0);
+
+
+        CGContextSetFillColor(context, CGColorGetComponents(color.CGColor))
+        CGContextFillRect(context, view.frame)
+        
+                CGContextSetBlendMode(context, kCGBlendModeOverlay)
+                CGContextDrawImage(context, view.frame, backgroundImage?.CGImage)
+        
+        let img = UIGraphicsGetImageFromCurrentImageContext()
+        UIGraphicsEndImageContext()
+        backgroundImageView.image = img
         view.addSubview(backgroundImageView)
-        view.addSubview(backgroundView)
-        view.sendSubviewToBack(backgroundView)
         view.sendSubviewToBack(backgroundImageView)
+//        CGContextRestoreGState(context)
+        
     }
 
     override func didReceiveMemoryWarning() {
