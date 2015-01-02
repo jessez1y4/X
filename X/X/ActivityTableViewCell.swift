7471ea1e67798db6788e3cdd8a2b821ab4b26e3b
yue zheng <zhengyuee@gmail.com>
2015-01-02 00:29:00 -0800

    activity view

---
+++ X/X/ActivityTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  ActivityTableViewCell.swift
+//  X
+//
+//  Created by yue zheng on 1/1/15.
+//  Copyright (c) 2015 PPC. All rights reserved.
+//
+
+import UIKit
+
+class ActivityTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
